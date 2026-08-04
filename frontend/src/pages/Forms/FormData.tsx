194f973eb49dc606beead0a99e65b5faa1da750c
Dawatenzingsherpa <dawatenzingsherpa907@gmail.com>
2026-08-04 10:17:34 +0545

    dynamic form

---
+++ frontend/src/pages/Forms/FormData.tsx
@@ -0,0 +1,104 @@
+export const employeeField = [
+  {
+    name: "firstName",
+    label: "First Name",
+    type: "text",
+    placeholder: "Enter first name",
+  },
+  {
+    name: "lastName",
+    label: "Last Name",
+    type: "text",
+    placeholder: "Enter last name",
+  },
+  {
+    name: "email",
+    label: "Email",
+    type: "email",
+    placeholder: "Enter email address",
+  },
+  {
+    name: "phoneNumber",
+    label: "Phone Number",
+    type: "text",
+    placeholder: "Enter phone number",
+  },
+  {
+    name: "hireDate",
+    label: "Hire Date",
+    type: "date",
+  },
+  {
+    name: "salary",
+    label: "Salary",
+    type: "number",
+    placeholder: "Enter salary",
+  },
+  {
+    name: "departmentId",
+    label: "Department",
+    type: "select",
+    options: ["23e7bde8-c1c3-4f5f-a6ea-3b3392e4556f"],
+  },
+];
+
+export const payrollField = [
+  {
+    name: "employeeId",
+    label: "Employee Id",
+    type: "text",
+  },
+];
+
+export const departmentField = [
+  {
+    name: "departmentName",
+    label: "Department Name",
+    type: "text",
+  },
+];
+
+export const performanceField = [
+  {
+    name: "attendence",
+    label: "Attendance",
+    type: "number",
+    placeholder: "Enter attendance score",
+  },
+  {
+    name: "quality",
+    label: "Quality",
+    type: "number",
+    placeholder: "Enter quality score",
+  },
+  {
+    name: "productivity",
+    label: "Productivity",
+    type: "number",
+    placeholder: "Enter productivity score",
+  },
+  {
+    name: "teamwork",
+    label: "Teamwork",
+    type: "number",
+    placeholder: "Enter teamwork score",
+  },
+  {
+    name: "employeeId",
+    label: "Employee",
+    type: "select",
+  },
+];
+
+export const leaveRequestField = [
+  {
+    name: "employeeId",
+    label: "employee Id",
+    type: "text",
+  },
+  {
+    name: "leaveDate",
+    label: "Leave Date",
+    type: "text",
+  },
+];
--- frontend/src/pages/Forms/FormElements.tsx
+++ frontend/src/pages/Forms/FormElements.tsx
@@ -1,17 +1,53 @@
 import PageBreadcrumb from "../../components/common/PageBreadCrumb";
 import DefaultInputs from "../../components/form/form-elements/DefaultInputs";
-import InputGroup from "../../components/form/form-elements/InputGroup";
-import DropzoneComponent from "../../components/form/form-elements/DropZone";
-import CheckboxComponents from "../../components/form/form-elements/CheckboxComponents";
-import RadioButtons from "../../components/form/form-elements/RadioButtons";
-import ToggleSwitch from "../../components/form/form-elements/ToggleSwitch";
-import FileInputExample from "../../components/form/form-elements/FileInputExample";
-import SelectInputs from "../../components/form/form-elements/SelectInputs";
-import TextAreaInput from "../../components/form/form-elements/TextAreaInput";
-import InputStates from "../../components/form/form-elements/InputStates";
+
 import PageMeta from "../../components/common/PageMeta";
+import {
+  departmentField,
+  employeeField,
+  leaveRequestField,
+  payrollField,
+  performanceField,
+} from "./FormData";
+import { useState } from "react";
 
 export default function FormElements() {
+  type FormType = keyof typeof forms;
+  const [SelectForm, setSelectForm] = useState<FormType>("employee");
+
+  const forms = {
+    employee: {
+      title: "Create Employee",
+      fields: employeeField,
+      submitText: "Save Employee",
+      onSubmit: () => console.log("employee"),
+    },
+
+    department: {
+      title: "Create Department",
+      fields: departmentField,
+      submitText: "Save Department",
+      onSubmit: () => console.log("departments"),
+    },
+    payroll: {
+      title: "Create Payroll",
+      fields: payrollField,
+      submitText: "create Payroll",
+      onSubmit: () => console.log("payroll"),
+    },
+    performance: {
+      title: "Create Performance",
+      fields: performanceField,
+      submitText: "create Performance",
+      onSubmit: () => console.log("performance"),
+    },
+    leaveRequest: {
+      title: "Create Leave Request",
+      fields: leaveRequestField,
+      submitText: "create Leave Request",
+      onSubmit: () => console.log("leaveRequest"),
+    },
+  };
   return (
     <div>
       <PageMeta
@@ -21,18 +57,18 @@ export default function FormElements() {
       <PageBreadcrumb pageTitle="Form Elements" />
       <div className="grid grid-cols-1 gap-6 xl:grid-cols-2">
         <div className="space-y-6">
-          <DefaultInputs />
-          <SelectInputs />
-          <TextAreaInput />
-          <InputStates />
-        </div>
-        <div className="space-y-6">
-          <InputGroup />
-          <FileInputExample />
-          <CheckboxComponents />
-          <RadioButtons />
-          <ToggleSwitch />
-          <DropzoneComponent />
+          <select
+            className="w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm text-gray-700 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
+            onChange={(e) => setSelectForm(e.target.value as FormType)}
+          >
+            <option value="">Select</option>
+            <option value="employee">Employee </option>
+            <option value="payroll">Payroll</option>
+            <option value="department">Department</option>
+            <option value="performance">Performance</option>
+            <option value="leaveRequest">Leave Request</option>
+          </select>
+          <DefaultInputs config={forms[SelectForm]} />
         </div>
       </div>
     </div>
